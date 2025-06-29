f7186628f107a260e1c992eacf16004166b0116b
khoihang123 <hangkhoii22102003@gmail.com>
2025-06-29 14:43:36 +0700

    14:43 / 29.06.2025

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT 
 const hostName = process.env.HOST_NAME
+const connection = require('./configs/database.js')
 
 app.get('/', (req, res) => {
   res.send('Hello World!!!??')
@@ -11,6 +12,12 @@ app.get('/', (req, res) => {
   res.send('Hello abc!!!!!')
 })
 
+connection.query('SELECT * FROM Users', 
+  function (error, results, fields) {
+    console.log('results', results);
+    //console.log('fields', fields);
+  }
+);
 app.listen(port, hostName, () => {
   console.log(`Example app listening on port ${port} and host ${hostName}`)
 })
